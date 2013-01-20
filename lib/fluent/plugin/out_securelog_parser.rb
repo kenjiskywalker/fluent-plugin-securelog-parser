ea3e428ad4f091dee7c59e1dcaf54d602013dac3
kenjiskywalker <git@kenjiskywalker.org>
2013-01-21 02:25:52 +0900

    コメントアウト周りの修正

--- lib/fluent/plugin/out_securelog_parser.rb
+++ lib/fluent/plugin/out_securelog_parser.rb
@@ -8,7 +8,7 @@ class Fluent::SecurelogParserOutput < Fluent::Output
   def initialize
     super
 
-    #インスタンス変数の初期化はここかな
+    # インスタンス変数の初期化はここかな
     @regexp = /(?<host>[^ ]+) (?<process>[^:]+): (?<key>[^*]+).*/
 
     # DATEフォーマット指定しておくとRegexpParserでパースする時に時間のパースをしなくて良いので有り難い
@@ -31,10 +31,7 @@ class Fluent::SecurelogParserOutput < Fluent::Output
   end
 
   def log_parse(value)
-
     # value           => "# Jan 17 02:47:59 hostname sshd[10654]: Received disconnect from 0.0.0.0: 11: Bye Bye"
-
-    #parser = Fluent::TextParser::RegexpParser.new(@regexp, 'time_format' => @time_format)
 
     parsed_value = @parser.call(value)
     # parsed_value    => [1358437767, {"host"=>"02:47:59", "process"=>"hostname sshd[10654]", "key"=>"Received disconnect from 0.0.0.0: 11: Bye Bye"}]
@@ -49,11 +46,9 @@ class Fluent::SecurelogParserOutput < Fluent::Output
 
       #recordの中身をkey, valueで分けない方法考えたい
       record.each do |key,value|
+
         message = log_parse(value)
-
-        #ここがアレ
         record = {"message" => message}
-
         Fluent::Engine.emit(@tag, time, record)
       end
     end
